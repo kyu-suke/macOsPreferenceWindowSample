9de0d9d9a5c1dff9d1e5cdc598f61b98b3c0ef26
kyu-suke <ykaneoka7@gmail.com>
2019-01-31 00:43:00 +0900

    ref:controller

--- macOsPreferenceWindowSample/TabViewController.swift
+++ macOsPreferenceWindowSample/TabViewController.swift
@@ -19,21 +19,11 @@ class TabViewController: NSTabViewController {
         super.toolbarDefaultItemIdentifiers(toolbar)
         
         var arr = Array<NSToolbarItem.Identifier>()
-        //        for item in self.tabViewItems {
-        //            if let identifier = item.identifier {
-        //                arr.append(NSToolbarItem.Identifier.init(identifier as! String))
-        //            }
-        //        }
         arr.append(NSToolbarItem.Identifier.init(self.tabViewItems[0].identifier as! String))
         arr.append(NSToolbarItem.Identifier.init(self.tabViewItems[1].identifier as! String))
         arr.append(NSToolbarItem.Identifier.flexibleSpace)
         arr.append(NSToolbarItem.Identifier.init(self.tabViewItems[2].identifier as! String))
         
-        //insert flexible spaces at first and last index
-        print(arr)
-        //        arr.insert(NSToolbarItem.Identifier.flexibleSpace, at: 0)
-        //        arr.append(NSToolbarItem.Identifier.flexibleSpace)
-        //
         return arr
     }
 
